268f070d4516a317fe812bb764103d7a2afa26e7
WATANA-be <aansssr@gmail.com>
2021-02-18 23:55:56 +0900

    totyuu

---
+++ デフォルト引数とミュータブル.py
@@ -0,0 +1,18 @@
+def create_int_list(numbers=[]):
+    for i in range(10):
+        numbers.append(i)
+    return numbers
+
+numbers = create_int_list()
+print(numbers)
+
+print()
+
+
+def create_int_list(numbers=[]):
+    for i in range(10):
+        numbers.append(i)
+    return numbers
+
+numbers = create_int_list([1,2,3])
+print(numbers)
