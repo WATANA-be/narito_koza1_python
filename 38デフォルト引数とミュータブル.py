af50c3db14b01a3ab1c3e0b1411c0080f78479fb
WATANA-be <aansssr@gmail.com>
2021-02-19 23:37:42 +0900

    難しい

---
+++ 38デフォルト引数とミュータブル.py
@@ -0,0 +1,35 @@
+def create_int_list(numbers=[]):
+    for i in range(10):
+        numbers.append(i)
+    return numbers
+
+numbers = create_int_list()
+print(numbers)
+
+print()
+
+
+def create_int_list(numbers=[]):
+    for i in range(10):
+        numbers.append(i)
+    return numbers
+
+numbers = create_int_list([1,2,3])
+print(numbers)
+
+
+def create_int_list(numbers=None):
+    if numbers is None:
+        numbers = []
+    for i in range(10):
+        numbers.append(i)
+    return numbers
+
+numbers = create_int_list()
+print(numbers)
+
+numbers2 = create_int_list
+print(numbers2)
+
+numbers3 = create_int_list
+print(numbers3)
