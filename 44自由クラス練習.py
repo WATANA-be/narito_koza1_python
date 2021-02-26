1f682906547041d559050d7361c460cf340d639f
WATANA-be <aansssr@gmail.com>
2021-02-26 23:17:47 +0900

    大事なので一旦自分で書いてみる

---
+++ 44自由クラス練習.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self,name):
+        self.name = name
+        self.dog_weight = {}
+    
+    def add_dog_weight(self,food_name,food_weight):
+        self.dog_weight[food_name]=food_weight
+        
+    def get_dog_weight(food_name,'そのエサはまだ')
+
+    pochi = Dog('pochi')
+    pochi.add_food_weight('beef',30)
+    print(pochi.dog_weight)
